dccb7ed5b1b1ddd2ba323701f320f51d0b16d2f4
Boon Low <boon.low@mac.com>
2018-08-23 16:01:41 +0100

    use ex_doc for documentation

--- mix.exs
+++ mix.exs
@@ -1,13 +1,28 @@
 defmodule Hui.MixProject do
   use Mix.Project
 
+  @description """
+    Hui is an Elixir client and library for interfacing with Solr enterprise search platform
+  """
+
   def project do
     [
       app: :hui,
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Hui",
+      description: @description,
+      source_url: "https://github.com/boonious/hui",
+      homepage_url: "https://github.com/boonious/hui",
+      docs: [
+        main: "Hui", # The main page in the docs
+        #logo: "path/to/logo.png",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -22,7 +37,8 @@ defmodule Hui.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.0"},
-      {:poison, "~> 3.1"}
+      {:poison, "~> 3.1"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
   end
 end
