4e8e18caecb3e4ff6fd29d1b9d1a9bb9c05db8a0
Boon Low <boon.low@mac.com>
2018-10-05 22:49:03 +0100

    refactor and additional test for live binary data update with “commitWithin”, “overwrite” parameters

--- test/update_live_test.exs
+++ test/update_live_test.exs
@@ -7,16 +7,25 @@ defmodule HuiUpdateLiveTest do
 
     test "should post binary data" do
       url = %Hui.URL{url: "http://localhost:8983/solr/gettingstarted", handler: "update", headers: [{"Content-type", "application/json"}]}
-      commit_doc = File.read!("./test/data/commit.xml")
       update_doc = File.read!("./test/data/update_doc1.json")
 
       delete_verify_doc_deletion(url, File.read!("./test/data/delete_doc1.json"), "9780141981727")
 
       Hui.Request.update(url,update_doc)
       url = %Hui.URL{url | headers: [{"Content-type", "application/xml"}]}
-      Hui.Request.update(url,commit_doc)
+      Hui.Request.update(url,"<commit/>")
 
       verify_docs_exist(:default, ["9780141981727"])
+    end
+
+    test "should post binary data with commitWithin and overwrite" do
+      url = %Hui.URL{url: "http://localhost:8983/solr/gettingstarted", handler: "update", headers: [{"Content-type", "application/json"}]}
+      delete_verify_doc_deletion(url, "{\"delete\":\"tt0062622\"}", "tt0062622")
+
+      Hui.Request.update(url, File.read!("./test/data/update_doc7.json"))
+      :timer.sleep(100)
+
+      verify_docs_exist(:default, ["tt0062622"])
     end
 
   end
@@ -26,18 +35,15 @@ defmodule HuiUpdateLiveTest do
 
     test "should post binary data" do
       url = %Hui.URL{url: "http://localhost:8983/solr/gettingstarted", handler: "update", headers: [{"Content-type", "application/xml"}]}
-      delete_doc = File.read!("./test/data/delete_doc2.xml")
-      commit_doc = File.read!("./test/data/commit.xml")
-      update_doc = File.read!("./test/data/update_doc2.xml")
 
       bang = true
-      Hui.Request.update(url, bang, delete_doc)
-      Hui.Request.update(url, bang, commit_doc)
+      Hui.Request.update(url, bang, File.read!("./test/data/delete_doc2.xml"))
+      Hui.Request.update(url, bang, "<commit/>")
       resp = Hui.search!(:default, q: "*", fq: ["id:9781910701874"])
       assert resp.body["response"]["numFound"] == 0
 
-      Hui.Request.update(url, bang, update_doc)
-      Hui.Request.update(url, bang, commit_doc)
+      Hui.Request.update(url, bang, File.read!("./test/data/update_doc2.xml"))
+      Hui.Request.update(url, bang, "<commit/>")
 
       verify_docs_exist(:default, ["9781910701874"])
     end
