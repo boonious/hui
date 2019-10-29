ffa9df27443b3f65e3f722f7ea4afd71ea9346a4
Boon Low <boon.low@mac.com>
2019-10-29 15:58:55 +0000

    remove tests of deprecated structs; type spec and code formatting

--- lib/hui/encode.ex
+++ lib/hui/encode.ex
@@ -3,13 +3,8 @@ defmodule Hui.Encode do
   Utilities for encoding Solr query and update data structures.
   """
 
-  alias Hui.Query
-
-  @type faceting_struct :: Query.Facet.t | Query.FacetRange.t | Query.FacetInterval.t
-  @type highlighting_struct :: Query.Highlight.t
-
-  @type solr_query :: Keyword.t | faceting_struct | highlighting_struct
-  @type options :: Options.t()
+  @type solr_query :: Hui.Encoder.query()
+  @type options :: Hui.Encode.Options.t()
 
   defmodule Options do
     defstruct [:per_field, :prefix, format: :url, separator: "&"]
@@ -25,44 +20,52 @@ defmodule Hui.Encode do
   @doc """
   Encodes list of Solr query keywords to IO data.
   """
-  @spec encode(solr_query) :: iodata
-  def encode(query) when is_list(query) do
+  @spec encode(solr_query, options) :: iodata
+  def encode(query, opts \\ %Options{})
+
+  def encode(query, _opts) when is_list(query) do
     query
-    |> Enum.reject(fn {k,v} -> is_nil(v) or v == "" or v == [] or k == :__struct__ end)
+    |> Enum.reject(fn {k, v} -> is_nil(v) or v == "" or v == [] or k == :__struct__ end)
     |> _encode
   end
 
   def encode(query, opts) when is_map(query) do
     query
-    |> Map.to_list
-    |> Enum.reject(fn {k,v} -> is_nil(v) or v == "" or v == [] or k == :__struct__ or k == :per_field end)
+    |> Map.to_list()
+    |> Enum.reject(fn {k, v} ->
+      is_nil(v) or v == "" or v == [] or k == :__struct__ or k == :per_field
+    end)
     |> _transform(opts)
     |> _encode(opts)
   end
 
-  defp _encode(query,  opts \\ %Options{})
-  defp _encode([head|[]], opts), do: [_encode(head, %{opts | separator: ""})]
-  defp _encode([head|tail], opts), do: [_encode(head, opts) | _encode(tail, opts)]
+  defp _encode(query, opts \\ %Options{})
+  defp _encode([head | []], opts), do: [_encode(head, %{opts | separator: ""})]
+  defp _encode([head | tail], opts), do: [_encode(head, opts) | _encode(tail, opts)]
 
   # do not render nil valued or empty keyword
-  defp _encode({_,nil}, _), do: ""
+  defp _encode({_, nil}, _), do: ""
   defp _encode([], _), do: ""
 
   # when value is a also struct, e.g. %Hui.Query.FacetRange/Interval{}
-  defp _encode({_,v}, opts) when is_map(v) do
+  defp _encode({_, v}, opts) when is_map(v) do
     sep = opts.separator
     if Map.has_key?(v, :__struct__), do: [Hui.Encoder.encode(v), sep], else: [encode(v), sep]
   end
 
   # encodes fq: [x, y] type keyword to "fq=x&fq=y"
-  defp _encode({k,v}, opts) when is_list(v), do: [ v |> Enum.map_join("&", &_encode( {k,&1}, %{opts | separator: ""} ) ), opts.separator ]
-  defp _encode({k,v}, opts), do: [to_string(k), "=", URI.encode_www_form(to_string(v)), opts.separator]
+  defp _encode({k, v}, opts) when is_list(v),
+    do: [v |> Enum.map_join("&", &_encode({k, &1}, %{opts | separator: ""})), opts.separator]
+
+  defp _encode({k, v}, opts),
+    do: [to_string(k), "=", URI.encode_www_form(to_string(v)), opts.separator]
 
   # render keywords according to Solr prefix / per field syntax
   # e.g. transform `field: "year"` into `"facet.field": "year"`, `f.[field].facet.gap` etc.
-  defp _transform([head|[]], opts), do: [_transform(head, opts)]
-  defp _transform([head|tail], opts), do: [_transform(head, opts) | _transform(tail, opts)]
-  defp _transform({k,v}, %Options{prefix: k_prefix, per_field: field}) do
+  defp _transform([head | []], opts), do: [_transform(head, opts)]
+  defp _transform([head | tail], opts), do: [_transform(head, opts) | _transform(tail, opts)]
+
+  defp _transform({k, v}, %Options{prefix: k_prefix, per_field: field}) do
     case {k, k_prefix, field} do
       {:facet, _, _} -> {:facet, v}
       {:hl, _, _} -> {:hl, v}
@@ -75,5 +78,4 @@ defmodule Hui.Encode do
       {_, _, _} -> {:"f.#{field}.#{k_prefix}.#{k}", v}
     end
   end
-
 end
--- lib/hui/encoder.ex
+++ lib/hui/encoder.ex
@@ -8,12 +8,17 @@ defprotocol Hui.Encoder do
 
   @type options :: keyword
 
-  @type querying_struct :: Query.Standard.t | Query.Common.t | Query.DisMax.t
-  @type faceting_struct :: Query.Facet.t | Query.FacetRange.t | Query.FacetInterval.t
-  @type highlighting_struct :: Query.Highlight.t
+  @type querying_struct :: Query.Standard.t() | Query.Common.t() | Query.DisMax.t()
+  @type faceting_struct :: Query.Facet.t() | Query.FacetRange.t() | Query.FacetInterval.t()
+  @type highlighting_struct ::
+          Query.Highlight.t()
+          | Query.HighlighterUnified.t()
+          | Query.HighlighterOriginal.t()
+          | Query.HighlighterFastVector.t()
+  @type misc_struct :: Query.MoreLikeThis.t() | Query.Suggest.t() | Query.SpellCheck.t()
 
-  @type solr_struct :: querying_struct | faceting_struct | highlighting_struct
-  @type query :: map | solr_struct
+  @type solr_struct :: querying_struct | faceting_struct | highlighting_struct | misc_struct
+  @type query :: Keyword.t() | map | solr_struct | [solr_struct]
 
   @doc """
   Transform `query` into IO data.
@@ -22,56 +27,74 @@ defprotocol Hui.Encoder do
   """
   @spec encode(query, options) :: iodata
   def encode(query, opts \\ [])
-
 end
 
 defimpl Hui.Encoder, for: [Query.Standard, Query.Common, Query.DisMax] do
-  def encode(query, _opts), do: Encode.encode( query|> Map.to_list ) |> IO.iodata_to_binary
+  def encode(query, _opts), do: Encode.encode(query |> Map.to_list()) |> IO.iodata_to_binary()
 end
 
 defimpl Hui.Encoder, for: Query.Facet do
-  def encode(query, _opts), do: Encode.encode(query, %Encode.Options{prefix: "facet"}) |> IO.iodata_to_binary
+  def encode(query, _opts),
+    do: Encode.encode(query, %Encode.Options{prefix: "facet"}) |> IO.iodata_to_binary()
 end
 
 defimpl Hui.Encoder, for: Query.FacetRange do
   def encode(query, _opts) do
-    options = %Encode.Options{prefix: "facet.range", per_field: (if query.per_field, do: query.range, else: nil )}
-    Encode.encode(query, options) |> IO.iodata_to_binary
+    options = %Encode.Options{
+      prefix: "facet.range",
+      per_field: if(query.per_field, do: query.range, else: nil)
+    }
+
+    Encode.encode(query, options) |> IO.iodata_to_binary()
   end
 end
 
 defimpl Hui.Encoder, for: Query.FacetInterval do
   def encode(query, _opts) do
-    options = %Encode.Options{prefix: "facet.interval", per_field: (if query.per_field, do: query.interval, else: nil )}
-    Encode.encode(query, options) |> IO.iodata_to_binary
+    options = %Encode.Options{
+      prefix: "facet.interval",
+      per_field: if(query.per_field, do: query.interval, else: nil)
+    }
+
+    Encode.encode(query, options) |> IO.iodata_to_binary()
   end
 end
 
-defimpl Hui.Encoder, for: [Query.Highlight, Query.HighlighterUnified, Query.HighlighterOriginal, Query.HighlighterFastVector] do
+defimpl Hui.Encoder,
+  for: [
+    Query.Highlight,
+    Query.HighlighterUnified,
+    Query.HighlighterOriginal,
+    Query.HighlighterFastVector
+  ] do
   def encode(query, _opts) do
-    options = %Encode.Options{prefix: "hl", per_field: (if query.per_field, do: query.field, else: nil )}
-    Encode.encode(query, options) |> IO.iodata_to_binary
+    options = %Encode.Options{
+      prefix: "hl",
+      per_field: if(query.per_field, do: query.field, else: nil)
+    }
+
+    Encode.encode(query, options) |> IO.iodata_to_binary()
   end
 end
 
 defimpl Hui.Encoder, for: Query.MoreLikeThis do
   def encode(query, _opts) do
     options = %Encode.Options{prefix: "mlt"}
-    Encode.encode(query, options) |> IO.iodata_to_binary
+    Encode.encode(query, options) |> IO.iodata_to_binary()
   end
 end
 
 defimpl Hui.Encoder, for: Query.Suggest do
   def encode(query, _opts) do
     options = %Encode.Options{prefix: "suggest"}
-    Encode.encode(query, options) |> IO.iodata_to_binary
+    Encode.encode(query, options) |> IO.iodata_to_binary()
   end
 end
 
 defimpl Hui.Encoder, for: Query.SpellCheck do
   def encode(query, _opts) do
     options = %Encode.Options{prefix: "spellcheck"}
-    Encode.encode(query, options) |> IO.iodata_to_binary
+    Encode.encode(query, options) |> IO.iodata_to_binary()
   end
 end
 
@@ -81,13 +104,13 @@ end
 
 defimpl Hui.Encoder, for: List do
   # encode a list of map or structs
-  def encode([x|y], _opts) when is_map(x) do
-    [x|y] |> Enum.map_join("&", &Hui.Encoder.encode(&1))
+  def encode([x | y], _opts) when is_map(x) do
+    [x | y] |> Enum.map_join("&", &Hui.Encoder.encode(&1))
   end
 
   # encode params in arbitrary keyword list
-  def encode([x|y], _opts) when is_tuple(x) do
-    URI.encode_query([x|y])
+  def encode([x | y], _opts) when is_tuple(x) do
+    URI.encode_query([x | y])
   end
 
   def encode([], _opts), do: ""
