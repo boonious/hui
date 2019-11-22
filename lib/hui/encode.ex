739e82050ae46e5b365a9b32e932d51a9ce78deb
Boon Low <boon.low@mac.com>
2019-11-22 15:21:35 +0000

    remove fixed chars from encoding options (struct) as these are implicit to the specified format (url, json)

--- lib/hui/encode.ex
+++ lib/hui/encode.ex
@@ -6,15 +6,15 @@ defmodule Hui.Encode do
   @type query :: Hui.Query.solr_query()
   @type options :: Hui.Encode.Options.t()
 
+  @url_delimiters {"=", "&"}
+
   defmodule Options do
-    defstruct [:per_field, :prefix, equal: "=", format: :url, separator: "&"]
+    defstruct [:per_field, :prefix, format: :url]
 
     @type t :: %__MODULE__{
-            equal: binary,
             format: :url,
             per_field: binary,
-            prefix: binary,
-            separator: binary
+            prefix: binary
           }
   end
 
@@ -24,13 +24,8 @@ defmodule Hui.Encode do
   @spec encode(query, options) :: iodata
   def encode(query, opts \\ %Options{})
 
-  def encode(query, _opts) when is_list(query) do
-    query
-    |> Enum.reject(fn {k, v} -> is_nil(v) or v == "" or v == [] or k == :__struct__ end)
-    |> _encode
-  end
-
-  def encode(query, opts) when is_map(query) do
+  # encode built-in query structs
+  def encode(%{__struct__: _} = query, opts) do
     query
     |> Map.to_list()
     |> Enum.reject(fn {k, v} ->
@@ -40,39 +35,51 @@ defmodule Hui.Encode do
     |> _encode(opts)
   end
 
-  defp _encode(query, opts \\ %Options{})
-  defp _encode([head | []], opts), do: [_encode(head, %{opts | separator: ""})]
-  defp _encode([head | tail], opts), do: [_encode(head, opts) | _encode(tail, opts)]
+  def encode(query, opts) when is_list(query) do
+    query
+    |> Enum.reject(fn {k, v} -> is_nil(v) or v == "" or v == [] or k == :__struct__ end)
+    |> _encode(opts)
+  end
+
+  def encode(query, opts) when is_map(query) do
+    query
+    |> Map.to_list()
+    |> Enum.reject(fn {k, v} -> is_nil(v) or v == "" or v == [] or k == :__struct__ end)
+    |> _encode(opts)
+  end
+
+  defp _encode(query, opts, delimiters \\ @url_delimiters)
+  defp _encode([h | []], opts, _), do: [_encode(h, opts, {"=", ""})]
+  defp _encode([h | t], opts, _), do: [_encode(h, opts) | _encode(t, opts)]
 
   # do not render nil valued or empty keyword
-  defp _encode({_, nil}, _), do: ""
-  defp _encode([], _), do: ""
+  defp _encode({_, nil}, _, _), do: ""
+  defp _encode([], _, _), do: ""
 
   # when value is a also struct, e.g. %Hui.Query.FacetRange/Interval{}
-  defp _encode({_, v}, opts) when is_map(v) do
-    sep = opts.separator
-    if Map.has_key?(v, :__struct__), do: [Hui.Encoder.encode(v), sep], else: [encode(v), sep]
+  defp _encode({_, v}, _opts, sep) when is_map(v) do
+    if Map.has_key?(v, :__struct__), do: [Hui.Encoder.encode(v)], else: [encode(v), sep]
   end
 
   # encodes fq: [x, y] type keyword to "fq=x&fq=y"
-  defp _encode({k, v}, opts) when is_list(v) do
+  defp _encode({k, v}, opts, {eql, sep}) when is_list(v) do
     [
       v
       |> Enum.reject(&(&1 == nil or &1 == ""))
-      |> Enum.map_join("&", &_encode({k, &1}, %{opts | separator: ""})),
-      opts.separator
+      |> Enum.map_join("&", &_encode({k, &1}, opts, {eql, ""})),
+      sep
     ]
   end
 
-  defp _encode({k, v}, opts),
-    do: [to_string(k), opts.equal, URI.encode_www_form(to_string(v)), opts.separator]
+  defp _encode({k, v}, _opts, {eql, sep}),
+    do: [to_string(k), eql, URI.encode_www_form(to_string(v)), sep]
 
   # render keywords according to Solr prefix / per field syntax
   # e.g. transform `field: "year"` into `"facet.field": "year"`, `f.[field].facet.gap` etc.
   defp _transform([head | []], opts), do: [_transform(head, opts)]
   defp _transform([head | tail], opts), do: [_transform(head, opts) | _transform(tail, opts)]
 
-  defp _transform({k, v}, %Options{prefix: k_prefix, per_field: per_field_field}) do
+  defp _transform({k, v}, %{prefix: k_prefix, per_field: per_field_field}) do
     cond do
       k_prefix && String.ends_with?(k_prefix, to_string(k)) -> {k_prefix, v}
       k_prefix && per_field_field == nil -> {:"#{k_prefix}.#{k}", v}
