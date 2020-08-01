9c8a5736f60e515f33842e95b8024da8c3ad47b7
Boon Low <boon.low@mac.com>
2020-08-01 16:07:04 +0100

    remove integration tests and data that depends on a live Solr instance

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
-ExUnit.start(exclude: [live: false])
+ExUnit.start()
 Application.ensure_all_started(:bypass)
 
 defmodule TestHelpers do
@@ -31,19 +31,6 @@ defmodule TestHelpers do
     assert String.match?(resp.url, regex)
   end
 
-  def test_all_search_live(query, expected_params, expected_url) do
-    {_, resp} = Hui.q(query)
-    requested_params = resp.body["responseHeader"]["params"]
-    assert expected_params == requested_params
-    assert String.match?(resp.url, expected_url)
-
-    {_, resp} = Hui.search(:default, query)
-    requested_params = resp.body["responseHeader"]["params"]
-    assert expected_params == requested_params
-    assert String.match?(resp.url, expected_url)
-  end
-
-  # for update tests
   def setup_bypass_for_post_req(bypass, expected_data, content_type \\ "application/json", resp \\ "") do
     Bypass.expect(bypass, fn conn ->
       assert String.match?(conn.request_path, ~r/\/update/)
@@ -55,37 +42,5 @@ defmodule TestHelpers do
 
       Plug.Conn.resp(conn, 200, resp)
     end)
-  end
-
-  # for live update tests
-  def delete_verify_doc_deletion(%Hui.URL{} = url, delete_msg, id) when is_binary(delete_msg) do
-    Hui.update(url, delete_msg)
-    Hui.update(url, %Hui.Query.Update{commit: true})
-    ids = if is_list(id), do: Enum.join(id, " OR "), else: id
-    {_, resp} = Hui.search(:default, q: "*", fq: ["id:(#{ids})"])
-    assert resp.body["response"]["numFound"] == 0
-  end
-
-  def delete_verify_doc_deletion(%Hui.URL{} = url, %Hui.Query.Update{} = delete_msg, id) do
-    Hui.update(url, delete_msg)
-    ids = if is_list(id), do: Enum.join(id, " OR "), else: id
-    {_, resp} = Hui.search(:default, q: "*", fq: ["id:(#{ids})"])
-    assert resp.body["response"]["numFound"] == 0
-  end
-
-  def verify_docs_exist(url, id) do
-    ids = if is_list(id), do: Enum.join(id, " OR "), else: id
-    {_, resp} = Hui.search(url, q: "*", fq: ["id:(#{ids})"])
-    assert resp.body["response"]["numFound"] == length(id)
-    docs = resp.body["response"]["docs"] |> Enum.map(&Map.get(&1, "id"))
-    for x <- id, do: assert(Enum.member?(docs, x))
-  end
-
-  def verify_docs_missing(url, id) do
-    ids = if is_list(id), do: Enum.join(id, " OR "), else: id
-    {_, resp} = Hui.search(url, q: "*", fq: ["id:(#{ids})"])
-    assert resp.body["response"]["numFound"] == 0
-    docs = resp.body["response"]["docs"] |> Enum.map(&Map.get(&1, "id"))
-    for x <- id, do: refute(Enum.member?(docs, x))
   end
 end
